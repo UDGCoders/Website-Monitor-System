12f3dbf7a040fb63cd8896f7b6c6acc1a9181761
UDGCoders <umarasif650@gmail.com>
2026-04-21 09:57:56 +0500

    Fixing Functions

--- index.py
+++ index.py
@@ -1,28 +1,26 @@
 import os
 import csv
 import requests
-from bs4 import BeautifulSoup
 from datetime import datetime   
 
 # ===========================================
 # variables for the connection to the site
 # ===========================================
+
 url="https://quotes.toscrape.com/"
 
-# timestamp=datetime.now()
 
 # ===========================================
 # connetcing and Fetching data 
 # ===========================================
+
 def fetch_data():
     try:
         response = requests.get(url)
         response.raise_for_status()
         log("Fetch Coompleted")
         return response.text
-        # if response.status_code == 200:
-        #     print("connection completed")
-        #     log("Connection Completed")
+
     except:
         print("Connection Failed")
         log("Connection Failed")
@@ -32,13 +30,14 @@ def fetch_data():
 # function for logging records
 # 1. check the directory if exists then ignore the command and moves to new line otherwise create a new file
 # ============================================
+
 def log(message):
     # os.makedirs("records",exist_ok=True)
     print(f"{datetime.now()} : {message}")
     try:
-        with open("file.csv","a",newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([f"{datetime.now()} : {message}"])
+        with open("logs.txt","a",newline='') as file:
+            
+            file.write(f"{datetime.now()} : {message} \n")
     except FileNotFoundError:
         print("File Not Found")
         # log("file not found")
@@ -46,46 +45,38 @@ def log(message):
 # ===========================================
 # reads all the files 
 # ===========================================
-def fetch_all():
+
+def load_old_data():
+    log("read mode for file")
     try:
-        with open("file.html","r",newline='',encoding="utf-8") as file:
+        with open("files/old.html","r",newline='',encoding="utf-8") as file:
             return file.read()
             
-        log("read mode for file")
     except FileNotFoundError:
         print("File not Found")
         log("File not found")
 
-# 
+# =========================================================================
 # Checks that file exists or not is exists then compare with new html file 
 # if not exists then create a new file 
-# 
+# =========================================================================
+
 def save_data(content):
     os.makedirs("files",exist_ok=True)
-    response=None
-    response=fetch_data()
-    if response != None:
-        soup=BeautifulSoup(response.text,"html.parser")
-        qoutes=soup.find_all("div",class_="quote")
-        text=soup.find_all("span",class_="text")
-        author=soup.find_all("small",class_="author")
-        try:
-            if os.path.exists("files/old_data.csv"):
-                with open("files/new_data.html","w",newline="",encoding="utf-8") as file:
-                    writer=csv.writer(file)
-                    file.write(content)
-                    
+   
+    try:
+        if os.path.exists("files/old.html"):
+            with open("files/new_data.html","w",newline="",encoding="utf-8") as file:                  
+                file.write(content)
+        else:
+            with open("files/old.html","w",newline="",encoding="utf-8") as file:
+                file.write(content)
+                
+    except FileNotFoundError:
+        print("File Not Found")
+        log("File Not Found")
 
-            else:
-                with open("files/old_data.csv",'w',newline="",encoding="utf-8")as file:
-                    writer=csv.writer(file)
-                    writer.writerow(["Author", "Quote"]) # Header
-                    for quote in qoutes:
-                        q_text = quote.find("span", class_="text").text
-                        q_author = quote.find("small", class_="author").text
-                        writer.writerow([q_author, q_text])
-        except FileNotFoundError:
-            print("File Not Found")
-            log("File Not Found")
 
-save_data()
+
+new_data = fetch_data()
+old_data = load_old_data()
